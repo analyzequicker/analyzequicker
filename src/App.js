990cebe32226d42309327bbcc6f359a273830dda
Jason Law <futbol10@users.noreply.github.com>
2022-12-31 14:56:18 -0500

    added a sample plotly chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Plot from "react-plotly.js";
+
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <div>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "red" },
+          },
+          { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+        ]}
+        layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+      />
+    </div>
+  );
 }
 
 export default App;
